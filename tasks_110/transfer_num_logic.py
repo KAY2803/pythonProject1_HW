1f3b506fedbdc928404f9931ff1cb72a4d403117
Anna Kim <kimspb85@mail.ru>
2021-12-01 00:11:56 +0300

    28/11/21

--- tasks_110/transfer_num_logic.py
+++ tasks_110/transfer_num_logic.py
@@ -7,3 +7,23 @@ def check_num():
             continue
         if isinstance(num_10, int):
             break
+    return num_10
+
+num_10 = check_num()
+num_2 = []
+
+def transfer():
+    global num_10
+    while True:
+        if num_10 % 2 == 0:
+            num_2.append(0)
+            num_10 = num_10 // 2
+        elif num_10 % 2 != 0:
+            num_2.append(1)
+            num_10 //= 2
+        if num_10 == 1:
+            num_2.append(1)
+            num_2_fin = int(''.join(map(str, num_2[::-1])))
+            break
+    return num_2_fin
+print(transfer())
