5ad5d22db5c9e77900c5d0b19a3e70efb0bc1e05
Anna Kim <kimspb85@mail.ru>
2021-12-04 00:05:02 +0300

    28/11/21

--- ht_3_27.11.21/task_2/task_2.py
+++ ht_3_27.11.21/task_2/task_2.py
@@ -1,10 +1,10 @@
 # Написать генератор, возвращающий по 3 символа из текстового файла, при этом не загружая в память весь файл.
 
 def gen_return(n: int):
-    with open('output.txt', 'r') as file:
-        for _ in range(100):
-            yield file.read(n)
+    with open('output.txt', 'r', encoding='utf8') as file:
+        for str_ in file.read():
+            yield str_[:3]
 
 symbols = gen_return(3)
-for _ in range(4):
-    print(next(symbols))
+for _ in range(12):
+    print(next(symbols), end='')
