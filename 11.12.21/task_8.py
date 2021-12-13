f5cc8a706519128944626d96077b195f3c5a9ca0
Anna Kim <kimspb85@mail.ru>
2021-12-14 02:29:10 +0300

    28/11/21

---
+++ 11.12.21/task_8.py
@@ -0,0 +1,32 @@
+# функция, которая текущее время конвертирует в формат заданный пользователем
+# time_from_pattern(time.time(), "YYYY-mm-dd") => "2021-12-22"
+import re
+import time
+import string
+
+def time_from_pattern(f_date:str):
+    t = time.time()
+    format_t = '-'.join(map(str, time.localtime(t)))
+    date_pattern = re.search(r'(?P<year>\b\d{4}\b)-(?P<month>\b\d{2}\b)-(?P<day>\b\d{2}\b)', format_t)
+    delimetr_pattern = re.search(r'[a-zA-Z]+(?P<delimetr>[\W+|_+|\s+])[a-zA-Z]+', f_date)
+    delimetr = delimetr_pattern.group(1)
+    format_date = date_pattern.group(1, 2, 3)
+    date = re.split('[-|,|:|;|\.|_|/]', f_date.lower())
+    for index, name in enumerate(date):
+        if 'y' in name:
+            date[index] = format_date[0]
+        elif 'm' in name:
+            date[index] = format_date[1]
+        elif 'd' in name:
+            date[index] = format_date[2]
+    final_date_pattern = delimetr.join(date)
+    # другой вариант
+    # final_date = f'{format_date[0]}-{format_date[1]}-{format_date[2]}'
+
+    time_ = time.strftime('%H:%M:%S', time.localtime(t))
+    return final_date_pattern, time_
+
+
+date_time = time_from_pattern("YYYY/mm/dd")
+print(f'date: {date_time[0]}')
+print(f'time: {date_time[1]}')
