142163f6fd9b30a6f98a95e83550ed879872a75d
Anna Kim <kimspb85@mail.ru>
2021-12-15 03:12:50 +0300

    28/11/21

---
+++ 11.12.21/task_3.py
@@ -0,0 +1,49 @@
+# генератор бесконечной волны, который будет "гонять" строку влево и вправо пока будет происходить вызов генератора.
+# Можно сделать не со строкой, а с символами, где текущий знак будет заменяться на другой символ
+
+## example 1     example 2
+"Hello"         "*---"
+"hEllo"         "-*--"
+"heLlo"         "--*-"
+"helLo"         "---*"
+"hellO"         "--*-"
+"helLo"         "-*--"
+"heLlo"         "*---"
+"hEllo"
+"Hello"
+
+def endless_gen(word: str):
+    word = word.lower()
+    one_way = range(len(word))
+    while True:
+        for i in one_way:
+            new_word = word[:i] + word[i].upper() + word[i + 1:]
+            yield new_word
+            if i == len(word)-2:
+                one_way = reversed(list(range(1, len(word)-1)))
+                i -= 1
+            if i == 1:
+                one_way = range(len(word))
+
+
+mygen = endless_gen('mmmmm')
+for _ in range(1):
+    print(next(mygen))
+
+
+def endless_gen(line: str):
+    one_way = range(len(line))
+    while True:
+        for i in one_way:
+            new_line = line[:i] + '*' + line[i + 1:]
+            yield new_line
+            if i == len(line)-2:
+                one_way = reversed(list(range(1, len(line)-1)))
+                i -= 1
+            if i == 1:
+                one_way = range(len(line))
+
+
+mygen = endless_gen('-----')
+for _ in range(10):
+    print(next(mygen))
